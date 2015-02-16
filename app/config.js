8d7f9a6d7b51aa6f1e2b915e25b2da297823b758
Alejandro U. Alvarez <aluralma@gmail.com>
2015-02-16 16:40:33 +0000

    Working frontend

--- app/config.js
+++ app/config.js
@@ -1,11 +1,22 @@
 /**
  * Strider
  * Requirejs configuration file
- * You shouldn't touch this file directly, instead run grunt
+ * You shouldn't touch the paths part of this file, instead run grunt
  * @type requirejs configuration
  */
 requirejs.config({
   shim: {
+    angular: {
+      exports: 'angular'
+    },
+    'angular-route': [
+      'angular'
+    ],
+    'angular-bootstrap': {
+      deps: [
+        'angular'
+      ]
+    },
     bootstrap: {
       deps: [
         'jquery'
@@ -13,11 +24,31 @@ requirejs.config({
     }
   },
   paths: {
+    app: 'app',
     requirejs: '../bower_components/requirejs/require',
     angular: '../bower_components/angular/angular',
-    'angular-bootstrap': '../bower_components/angular-bootstrap/ui-bootstrap-tpls'
+    'angular-bootstrap': '../bower_components/angular-bootstrap/ui-bootstrap-tpls',
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
+    'angular-loader': '../bower_components/angular-loader/angular-loader',
+    'angular-route': '../bower_components/angular-route/angular-route'
   },
   packages: [
 
   ]
+});
+
+require([
+  'angular',
+  'app'
+], function(angular, app) {
+
+  console.info("Stride Web initialized");
+
+  var $html = angular.element(document.getElementsByTagName('html')[0]);
+
+  angular.element().ready(function() {
+    // bootstrap the app manually
+    angular.bootstrap(document, ['myApp']);
+  });
+
 });
