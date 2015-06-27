17f85b1c33c5bd1cd9144dfedf7dd076788f5c7d
Alejandro U. Alvarez <aluralma@gmail.com>
2015-06-27 15:46:48 +0100

    Display items stats

--- app/organisations/dashboard/items/itemsService.js
+++ app/organisations/dashboard/items/itemsService.js
@@ -4,7 +4,7 @@ define(['angular'], function(angular) {
   angular.module('services.items', [])
     .service('Items', ['$resource', 'apiUrl',
       function($resource, apiUrl) {
-        return $resource(apiUrl + ':org/items/:id');
+        return $resource(apiUrl + 'organisations/:org/items/:id');
       }
     ]);
 });
--- app/organisations/dashboard/landing/landingCtrl.js
+++ app/organisations/dashboard/landing/landingCtrl.js
@@ -2,9 +2,16 @@ define(['angular'], function(angular) {
   'use strict';
 
   angular.module('LandingCtrls', [])
-    .controller('LandingCtrl', ['$scope', 'UserOrgs',
-      function($scope, UserOrgs) {
+    .controller('LandingCtrl', ['$scope', 'Items',
+      function($scope, Items) {
         console.log("Landing Controller");
+
+        $scope.counts = {
+          items: Items.get({
+            org: $scope.Org.id,
+            id: 'count'
+          })
+        };
       }
     ]);
 });
