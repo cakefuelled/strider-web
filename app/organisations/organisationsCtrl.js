a97dc80a1e073ff040279b52237d3ee594cd244a
Alejandro U. Alvarez <aluralma@gmail.com>
2015-07-05 10:20:18 +0100

    Playing with different login window styles

--- app/organisations/organisationsCtrl.js
+++ app/organisations/organisationsCtrl.js
@@ -1,21 +1,35 @@
 define(['angular'], function(angular) {
   'use strict';
-  
+
   angular.module('OrganisationCtrls', [])
-    .controller('OrganisationCtrl', ['$scope', '$http', 'apiUrl', 'UserOrgs',
-      function($scope, $http, apiUrl, UserOrgs) {
+    .controller('OrganisationCtrl', ['$scope', '$http', 'apiUrl', 'UserOrgs', 'store', 'User',
+      function($scope, $http, apiUrl, UserOrgs, store, User) {
         console.log('Organisation ctrl');
 
         $scope.organisations = UserOrgs.query({
           id: 'me'
         });
 
-        $scope.organisations.$promise.then(function(){}, function(err){
-          if(err.status === 404){
+        $scope.mainClass = 'backdrop backdrop-payphone';
+
+        $scope.organisations.$promise.then(function() {}, function(err) {
+          if (err.status === 404) {
             // User not logged in
-            $scope.$emit('event:auth-loginRequired',{});
+            $scope.$emit('event:auth-loginRequired', {});
           }
         });
+
+        $scope.logout = function() {
+          User.logout().$promise.then(function() {
+            console.log('Logged out');
+            store.remove('accessToken');
+            delete $http.defaults.headers.common.Authorization;
+
+            $scope.organisations = UserOrgs.query({
+              id: 'me'
+            });
+          });
+        };
       }
     ]);
 });
